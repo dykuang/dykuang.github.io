6540f3a7bb771533d0ddad944034d5c66680715a
Dongyang Kuang <dykuangii@gmail.com>
2018-01-05 14:23:46 -0500

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Pittsburgh, Pennsylvania, USA",
+    40.4416941,
+    -79.9900861
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Laramie, WY",
+    41.311367,
+    -105.591101
   ]
 ];
