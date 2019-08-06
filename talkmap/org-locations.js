a828b7af3a49144582cdc3917054ad740dc9f634
Dongyang Kuang <dykuangii@gmail.com>
2019-08-05 20:15:28 -0500

    update talkmap

---
+++ talkmap/org-locations.js
@@ -0,0 +1,22 @@
+var addressPoints = [
+  [
+    "Banff, Alberta, Canada",
+    51.1777781,
+    -115.5682504
+  ],
+  [
+    "Pittsburgh, Pennsylvania, USA",
+    40.4416941,
+    -79.9900861
+  ],
+  [
+    "Ottawa, Ontario, Canada",
+    45.421106,
+    -75.690308
+  ],
+  [
+    "Laramie, WY",
+    41.311367,
+    -105.591101
+  ]
+];
