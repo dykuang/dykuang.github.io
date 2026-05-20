1de1a8f4ee0946bd17282ee7f50a3e90d9d1f76b
Dongyang Kuang <dykuangii@gmail.com>
2026-05-20 22:34:12 +0800

    Update coordinates in org-locations.js for accuracy

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,52 @@
 var addressPoints = [
   [
     "Banff, Alberta, Canada",
-    51.1777781,
-    -115.5682504
+    51.1784,
+    -115.5708
   ],
   [
     "Pittsburgh, Pennsylvania, USA",
-    40.4416941,
-    -79.9900861
+    40.4406,
+    -79.9959
+  ],
+  [
+    "Zhuhai, CHINA",
+    22.271,
+    113.5767
+  ],
+  [
+    "Guangzhou, CHINA",
+    23.1291,
+    113.2644
+  ],
+  [
+    "Nanjing, CHINA",
+    32.0603,
+    118.7969
+  ],
+  [
+    "Singapore, SINGAPORE",
+    1.3521,
+    103.8198
+  ],
+  [
+    "ShenZhen, CHINA",
+    22.5431,
+    114.0579
   ],
   [
     "1920 Research Private, Ottawa, Ontario, Canada",
-    45.325311,
-    -75.6659056114435
+    45.4215,
+    -75.6972
   ],
   [
     "Laramie, WY",
-    41.311367,
-    -105.591101
+    41.3114,
+    -105.5911
   ],
   [
     "Ottawa, Ontario, Canada",
-    45.421106,
-    -75.690308
+    45.4215,
+    -75.6972
   ]
 ];
